75e4bd67eb4d18bbf8ab3ef4ad095bf1d98dd0df
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-24 07:34:27 +0530

    Creating services - Geolocation and Maps
    navigator.geolocation.getCurrentPosition()
    navigator.userAgent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { GeolocationService } from './geolocation.service';
 
 
 @NgModule({
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ GeolocationService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/geolocation.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GeolocationService {
+
+  constructor() { }
+
+  requestLocation(callback) {
+    // W3C Geolocation API
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        callback(position.coords);
+      },
+      error => {
+        callback(null);
+      }
+    );
+  }
+
+  getMapLink(location: PlaceLocation) {
+    // Universal Link
+    // <a href="https://maps.google.com/?q=Eiffle+Tower">
+    // <a href="https://maps.google.com/?q=33.66,44.56">
+    // <a href="https://maps.apple.com/?q=33.66,44.56">
+
+    let query = "";
+    if(location.latitude) {
+      query = location.latitude + ',' + location.longitude;
+    } else {
+      query = `${location.address}, ${location.city}`;
+    }
+
+    if((/iPad | iPhone | iPod/).test(navigator.userAgent)) {
+      return `https://maps.apple.com/?q=${query}`;
+    } else {
+      return `https://maps.google.com/?q=${query}`;
+    }
+  }
+
+}
