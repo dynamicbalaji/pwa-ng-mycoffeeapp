5bd3855de60a618d884f58250d9ab6320e1b6581
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-25 21:38:41 +0530

    Creating components - List

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Coffee } from '../logic/Coffee';
+import { DataService } from '../data.service';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  list: [Coffee];
+
+  constructor(private _dataService: DataService) { }
 
   ngOnInit() {
+    this._dataService.getList(list => {
+      this.list = list;
+    });
   }
 
 }
