a4d87747c3b0be1d1666fad22949357f09b88a4b
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-25 20:46:47 +0530

    Adding material dependencies in the project
    Import material modules in app module
    Add in-build theme in styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule,
+         MatSliderModule, MatToolbarModule, MatCardModule, MatSlideToggleModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,7 +18,10 @@ import { DataService } from "./data.service";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatIconModule, MatInputModule, MatSelectModule,
+    MatSliderModule, MatToolbarModule, MatCardModule, MatSlideToggleModule
   ],
   providers: [ GeolocationService, DataService ],
   bootstrap: [AppComponent]
