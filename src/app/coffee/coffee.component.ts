91dea7188cf9b1f0d80b87800eb752fdcb4c2e56
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-12-01 08:09:29 +0530

    Added lat & lon from geolocation service

--- src/app/coffee/coffee.component.ts
+++ src/app/coffee/coffee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Coffee } from '../logic/Coffee';
+import { GeolocationService } from '../geolocation.service';
 
 @Component({
   selector: 'app-coffee',
@@ -13,7 +14,8 @@ export class CoffeeComponent implements OnInit {
   coffee: Coffee;
   types: string[] = ['Espresso', 'Cappuccino', 'Ristretto', 'Americano', 'Frappe'];
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute,
+              private _geolocation: GeolocationService) { }
 
   routingSubscription: any;
 
@@ -23,6 +25,10 @@ export class CoffeeComponent implements OnInit {
           this._route.params.subscribe((params) => {
             console.log(params['id']);
           });
+    this._geolocation.requestLocation(location => {
+      this.coffee.location.latitude = location.latitude;
+      this.coffee.location.longitude = location.longitude;
+    });
   }
 
   ngOnDestroy() {
