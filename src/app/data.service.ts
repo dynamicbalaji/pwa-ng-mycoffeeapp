efd0029ba6f61b7204c1d47691bd7ea7a5c07b6b
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-12-03 21:18:06 +0530

    Modified data service to use RESTFul API
    `${this.endpoint}/coffees`
    coffee._id - _id is a property created by nedb for us, if it is there, then its an update or else insert

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,21 +7,36 @@ import { PlaceLocation } from "./logic/PlaceLocation";
 @Injectable()
 export class DataService {
 
+  endpoint: string = "http://localhost:3000";
+
   constructor(private _http: Http) { }
 
   getList(callback) {
-    // TODO: Change it with a real Web Service
-    let list = [
+    /* let list = [
       new Coffee("Double Espresso", "Sunny Cafe",  new PlaceLocation("123 Market St", "San Francisco")),
       new Coffee("Caramel Americano", "Starcoffee",  new PlaceLocation("Gran Via 34", "Madrid"))
     ];
 
-    callback(list);
+    callback(list); */
+    this._http.get(`${this.endpoint}/coffees`).subscribe(response => {
+      callback(response.json());
+    });
   }
-
-  save(callback) {
-    // TODO: Change it with a real Web Service
-    callback(true);
+  
+  save(coffee, callback) {
+    /* callback(true); */
+    // _id is a property created by nedb for us. If its there, then its an update
+    if(coffee._id){
+      // Update
+      this._http.put(`${this.endpoint}/coffees/${coffee._id}`, coffee).subscribe(response => {
+        callback(true);
+      });
+    } else {
+      // Insert
+      this._http.post(`${this.endpoint}/coffees`, coffee).subscribe(response => {
+        callback(true);
+      });
+    }
   }
 
 }
